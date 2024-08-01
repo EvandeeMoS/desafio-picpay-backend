3dcc8f33d56868f0135cc7d9eed7e4b85c6b06f5
EvandeeMoS <moisesevander@gmail.com>
2024-07-31 22:35:03 -0300

    feat: add user entity

---
+++ src/main/java/com/evandeemos/desafiopicpay/domain/user/User.java
@@ -0,0 +1,28 @@
+package com.evandeemos.desafiopicpay.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "Users")
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String document;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
