5a8f2d500afeea13b98ca9377fa3cc1126a9e856
EvandeeMoS <moisesevander@gmail.com>
2024-08-05 08:46:33 -0300

    feat: add Transaction Entity

--- src/main/java/com/evandeemos/desafiopicpay/domain/transaction/Transaction.java
+++ src/main/java/com/evandeemos/desafiopicpay/domain/transaction/Transaction.java
@@ -6,6 +6,7 @@ import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Entity(name = "Transactions")
 @Table(name = "transactions")
@@ -26,4 +27,11 @@ public class Transaction {
     @JoinColumn(name = "payee_id")
     private User payee;
     private LocalDateTime timestamp;
+
+    public Transaction(BigDecimal amount, User payer, User payee) {
+        this.amount = amount;
+        this.payer = payer;
+        this.payee = payee;
+        this.timestamp = LocalDateTime.now(ZoneId.of("America/Recife"));
+    }
 }
